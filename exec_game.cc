5b9325334ca430c607e7017e980eb46fe175e47d
Bogu Wei <bogu@airtime.com>
2016-11-12 14:16:25 -0800

    1_Program_Options: refactor out all param code
    Remove argc/argv implementation in preparation for new program Options class
    Refactor variable names to conform to Google style

--- exec_game.cc
+++ exec_game.cc
@@ -7,35 +7,37 @@
 #include "game_session.h"
 
 int main(int argc, char* argv[]) {
-  if (argc < 4) {
-    std::cout << "Incorrect number of inputs " << std::endl;
-    return 1;
-  }
-  int playerCount = std::atoi(argv[1]);
-  int sticksCount = 0;
-  if (argc > sticksCount+1){
-    sticksCount = std::atoi(argv[playerCount + 2]);
-  }
-  std::vector<std::string> listOfNames;
-  std::vector<int> sticksAllowed;
-  for (int i = 2; i < playerCount + 2; ++i) {
-    listOfNames.emplace_back(argv[i]);
-  }
-  for (int i = playerCount+3; i < argc; ++i) {
-    sticksAllowed.emplace_back(std::atoi(argv[i]));
-  }
+  /*
+      if (argc < 4) {
+        std::cout << "Incorrect number of inputs " << std::endl;
+        return 1;
+      }
+      int playerCount = std::atoi(argv[1]);
+      int sticks_count = 0;
+      if (argc > sticksCount+1){
+        sticksCount = std::atoi(argv[playerCount + 2]);
+      }
+      std::vector<std::string> names_list;
+      std::vector<int> sticks_allowed;
+      for (int i = 2; i < playerCount + 2; ++i) {
+        names_list.emplace_back(argv[i]);
+      }
+      for (int i = playerCount+3; i < argc; ++i) {
+        sticks_allowed.emplace_back(std::atoi(argv[i]));
+      }
+  */
 
   std::unique_ptr<gameofsticks::Session> the_game;
-  if (sticksCount && !sticksAllowed.empty()) {
+  if (sticks_count && !sticks_allowed.empty()) {
     the_game = std::unique_ptr<gameofsticks::Session>(
             new gameofsticks::Session(gameofsticks::GameType::PLAYER_V_PLAYER,
-                                      listOfNames,
-                                      sticksAllowed,
-                                      sticksCount));
+                                      names_list,
+                                      sticks_allowed,
+                                      sticks_count));
   
   } else {
     the_game = std::unique_ptr<gameofsticks::Session>(
-            new gameofsticks::Session(gameofsticks::GameType::PLAYER_V_PLAYER, listOfNames));
+            new gameofsticks::Session(gameofsticks::GameType::PLAYER_V_PLAYER, names_list));
   }
 
   the_game -> PrintSessionStats();
