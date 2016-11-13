57783887bbb401b8a86842aecef1bffe17c3d68c
Bogu Wei <bogu@airtime.com>
2016-11-13 11:27:16 -0800

    1_Program_Options: refactor session stats code
    Remove indent for player names
    Adjust commented legacy code to be inline

--- game_session.cc
+++ game_session.cc
@@ -29,20 +29,21 @@ std::string Session::ToString(GameType type) {
 }
 
 void Session::PrintSessionStats() {
-/*
-  if (player_one_ && player_two_) {
-    std::cout << "Game session of type [" << ToString(game_type_) << "] with players:" << std::endl;
-    std::cout << "    " << player_one_ -> name_ << std::endl;
-    player_one_ -> PrintMemory();
-    std::cout << "    " << player_two_ -> name_ << std::endl;
-    player_two_ -> PrintMemory();
-  } else {
-    std::cout << "Game session incomplete." << std::endl;
-  }
-*/
+  /*
+    if (player_one_ && player_two_) {
+      std::cout << "Game session of type [" << ToString(game_type_) << "] with players:" << std::endl;
+      std::cout << "    " << player_one_ -> name_ << std::endl;
+      player_one_ -> PrintMemory();
+      std::cout << "    " << player_two_ -> name_ << std::endl;
+      player_two_ -> PrintMemory();
+    } else {
+      std::cout << "Game session incomplete." << std::endl;
+    }
+  */
+
   std::cout << "Game session of type [" << ToString(game_type_) << "] with players:" << std::endl;
   for(int i = 0; i < players_.size(); ++i) {
-    std::cout << "    " << players_[i] -> name_ << std::endl;
+    std::cout << players_[i] -> name_ << std::endl;
     players_[i] -> PrintMemory();
   }
   PrintTestStats();
